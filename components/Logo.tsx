b25d7e388592d490cafa9d44f1ac7f68c59c3626
Ihar Voitau <igorgrime@gmail.com>
2025-02-05 00:53:53 +0300

    feat: add NavLinks

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
-// import {calSans} from "@/app/fonts"
 import { SwitchCamera } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+// import { calSans } from "@/app/fonts";
 
 function Logo() {
   return (
@@ -9,7 +9,7 @@ function Logo() {
       href={"/dashboard"}
       className={buttonVariants({
         className:
-          "hidden md:flex navLink !mb10 lg:hover:bg-transparent lg:!p-0",
+          "hidden md:flex navLink !mb-10 lg:hover:bg-transparent lg:!p-0",
         variant: "ghost",
         size: "lg",
       })}
