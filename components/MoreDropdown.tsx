809e201a7bb7921514e2f75e95e69771c1f74e03
HAROSHII <igorgrime@gmail.com>
2025-03-04 14:04:50 +0300

    feat: creating a dropdown menu component with client-side button trigger

--- components/MoreDropdown.tsx
+++ components/MoreDropdown.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { cn } from "@/lib/utils";
 
 function MoreDropdown() {
-  return <div>MoreDropdown</div>;
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Close the dropdown when the user clicks outside
+    function handleOutsideClick(event: MouseEvent) {
+      if (!event.target) return;
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        // setShowModeToggle(false);
+        setOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [ref]);
+
+  return (
+    <DropdownMenu open={open}>
+      <DropdownMenuTrigger asChild>
+        <Button
+          onClick={() => setOpen(!open)}
+          variant={"ghost"}
+          size={"lg"}
+          className="md:w-full md:!justify-center lg:!justify-start space-x-2 !px-3"
+        >
+          <Menu />
+          <div className="hidden lg:block">More</div>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        ref={ref}
+        className={cn(
+          "dark:bg-neutral-800 w-64 !rounded-xl !p-0 transition-opacity",
+          !open && "opacity-0"
+        )}
+        align="end"
+        alignOffset={-40}
+      >
+        <DropdownMenuItem>
+          <DropdownMenuLabel>Settings</DropdownMenuLabel>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <DropdownMenuLabel>Help</DropdownMenuLabel>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <DropdownMenuLabel>Logout</DropdownMenuLabel>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default MoreDropdown;
